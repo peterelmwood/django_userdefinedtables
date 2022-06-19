fc9573289d12f25f162a5fdf6cf3706d16ebcfbe
Peter Wood <peterelmwood@gmail.com>
2022-06-19 17:57:11 +0000

    added models and tests

--- userdefinedtables/models.py
+++ userdefinedtables/models.py
@@ -1,6 +1,7 @@
 from typing import List as TypedList
 
 from django.db import models
+from django.db.models.functions import Length
 
 
 class List(models.Model):
@@ -29,15 +30,46 @@ class Column(models.Model):
 
 
 class SingleLineOfTextColumn(Column):
-    value = models.CharField(max_length=255, blank=True, null=False)
     maximum_length = models.PositiveSmallIntegerField(default=255)
 
-    class Meta(Column.Meta):
-        constraints = Column.Meta.constraints + [
+    class Meta:
+        constraints = [
             models.CheckConstraint(check=models.Q(maximum_length__lte=255), name="maximum_length cannot exceed 255"),
-            models.UniqueConstraint(
-                fields=["value", "list"],
-                condition=models.Q(unique=True),
-                name="value cannot occur twice in list, if unique == True",
-            ),
         ]
+
+    def save(self, *args, **kwargs) -> None:
+        if not self._state.adding:
+            entries_with_value_length = self.entries.annotate(length=Length("value"))
+            if entries_with_value_length.exists():
+                longest_entry = max(entries_with_value_length.values_list("length", flat=True))
+                if longest_entry > self.maximum_length:
+                    raise ValueError("Cannot reduce maximum_length property below the longest entry's length.")
+        super().save(*args, **kwargs)
+
+
+class SingleLineOfTextColumnEntry(models.Model):
+    value = models.CharField(max_length=255, blank=True, null=False)
+
+    column = models.ForeignKey(
+        "userdefinedtables.singlelineoftextcolumn", null=False, on_delete=models.CASCADE, related_name="entries"
+    )
+
+    def save(self, *args, **kwargs) -> None:
+        if self.column.maximum_length < len(self.value):
+            raise ValueError("Text entry length cannot exceed column maximum_length property.")
+        super().save(*args, **kwargs)
+
+
+class MultipleLineTextColumn(Column):
+    pass
+
+
+class MultipleLineTextColumnEntry(models.Model):
+    value = models.TextField(blank=True, null=False)
+
+    column = models.ForeignKey(
+        "userdefinedtables.multiplelinetextcolumn", null=False, on_delete=models.CASCADE, related_name="entries"
+    )
+
+
+class 
--- userdefinedtables/tests.py
+++ userdefinedtables/tests.py
@@ -1,3 +1,49 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from model_bakery import baker
+
+
+class ColumnTestCase(TestCase):
+    def test__list_cannot_have_two_identically_named_columns(self):
+        # ASSIGN
+        list_1 = baker.make("userdefinedtables.list")
+        column_1 = baker.make(
+            "userdefinedtables.column",
+            list=list_1,
+            name="duplicate",
+        )
+
+        # ACT
+        # ASSERT
+        with self.assertRaises(IntegrityError):
+            baker.make("userdefinedtables.column", list=list_1, name=column_1.name)
+
+
+class SingleLineOfTextColumnTestCase(TestCase):
+    def test__singlelineoftextcolumn_maximum_length_cannot_exceed_max_length_of_255(self):
+        # ASSIGN
+        # ACT
+        # ASSERT
+        with self.assertRaises(IntegrityError):
+            baker.make("userdefinedtables.singlelineoftextcolumn", maximum_length=256)
+
+    def test__singlelineoftextcolumn_maximum_length_cannot_be_decreased_below_longest_entry(self):
+        # ASSIGN
+        column = baker.make("userdefinedtables.singlelineoftextcolumn", maximum_length=255)
+        entry = baker.make("userdefinedtables.singlelineoftextcolumnentry", column=column, value="1" * 255)
+
+        # ACT
+        # ASSERT
+        with self.assertRaises(ValueError):
+            column.maximum_length = len(entry.value) - 1
+            column.save()
+
+    def test__singlelineoftextcolumnentry_cannot_be_exceed_singlelineoftextcolumn_maximum_length(self):
+        # ASSIGN
+        column = baker.make("userdefinedtables.singlelineoftextcolumn", maximum_length=255)
+
+        # ACT
+        # ASSERT
+        with self.assertRaises(ValueError):
+            entry = baker.make("userdefinedtables.singlelineoftextcolumnentry", column=column, value="1" * 256)
