801c5cc51f7477904afa500efdefcba4b06b5758
Peter Wood <peterelmwood@gmail.com>
2022-07-23 22:58:03 +0000

    renamed appconfig

--- example/apps/userplayground/apps.py
+++ example/apps/userplayground/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ToysConfig(AppConfig):
+class UserPlaygroundConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "example.apps.userplayground"
