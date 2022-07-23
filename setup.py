e728d5e2c800d89e1c08cfdd5decdcd6e53d4e70
Peter Wood <peterelmwood@gmail.com>
2022-07-23 15:45:30 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def long_desc(root_path):
+    FILES = ["README.md"]
+    for filename in FILES:
+        filepath = os.path.realpath(os.path.join(root_path, filename))
+        if os.path.isfile(filepath):
+            with open(filepath, mode="r") as f:
+                yield f.read()
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+long_description = "\n\n".join(long_desc(HERE))
+
+
+setup(
+    name="django_userdefinedtables",
+    use_scm_version={"version_scheme": "post-release"},
+    setup_requires=["setuptools_scm"],
+    license="MIT",
+    description="Generic EAV-style table creation in the user's hands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Peter Wood",
+    author_email="peterelmwood@gmail.com",
+    maintainer="Peter Wood",
+    url="https://github.com/peterelmwood/django_userdefinedtables",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=["Django>=2.2"],
+    classifiers=[
+        "Development Status :: 1 - pLANNING",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: mit License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+    ],
+    zip_safe=False,
+)
