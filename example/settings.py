28577d07d2d5fc149b7df24fff07c09b7d49bac8
Peter Wood <peterelmwood@gmail.com>
2022-07-23 15:30:29 +0000

    minor example settings changes

--- example/settings.py
+++ example/settings.py
@@ -2,11 +2,10 @@
 Django settings for django_userdefinedtables project.
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # this app
     "userdefinedtables",
 ]
 
@@ -61,16 +61,13 @@ TEMPLATES = [
     },
 ]
 
-# WSGI_APPLICATION = "django_userdefinedtables.wsgi.application"
-
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'udt_example',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "udt_example",
     }
 }
 
