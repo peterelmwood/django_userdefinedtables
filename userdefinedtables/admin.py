7c7d5adb8fae5031a97596521512d43a344edc00
Peter Wood <peterelmwood@gmail.com>
2022-07-25 05:09:13 +0000

    added different column types to admin

--- userdefinedtables/admin.py
+++ userdefinedtables/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from userdefinedtables.models import Column, List
+from userdefinedtables.models import *
 
 
 @admin.register(List)
@@ -10,4 +10,54 @@ class ListAdmin(admin.ModelAdmin):
 
 @admin.register(Column)
 class ColumnAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ChoiceColumn)
+class ChoiceColumnAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BinaryColumn)
+class BinaryColumnAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(LookupColumn)
+class LookupColumnAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NumberColumn)
+class NumberColumnAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PictureColumn)
+class PictureColumnAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CurrencyColumn)
+class CurrencyColumnAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DateTimeColumn)
+class DateTimeColumnAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MultipleLineTextColumn)
+class MultipleLineTextColumnAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SingleLineOfTextColumn)
+class SingleLineOfTextColumnAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(URLColumn)
+class URLColumnAdmin(admin.ModelAdmin):
     pass
